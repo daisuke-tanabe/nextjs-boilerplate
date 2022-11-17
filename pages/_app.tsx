6d198dc9d3eba9e3db06d3fdc778a186723aebe3
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2022-11-17 23:25:28 +0900

    feat: setup mui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import createEmotionCache from '../lib/createEmotionCache';
+import theme from '../lib/theme';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+type MyAppProps = AppProps & {
+  emotionCache?: EmotionCache;
+};
+
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) => (
+  <CacheProvider value={emotionCache}>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </CacheProvider>
+);
+
+export default MyApp;

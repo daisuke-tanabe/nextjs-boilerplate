002c7c73c084c68703e8969560083e9e8993e750
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2022-11-21 02:40:10 +0900

    chore: add example graphql

--- apollo/resolvers.ts
+++ apollo/resolvers.ts
@@ -1,23 +1,39 @@
+const books = [
+  {
+    id: '1',
+    title: 'book - 1',
+    creators: [
+      { id: '1', name: 'ななしのごんべえ', role: '著者' },
+      { id: '2', name: 'なありのごんべえ', role: '著者' }
+    ],
+    image: { small: '', medium: '', large: '' },
+    stock: 3,
+    users: [
+      { id: '1', name: 'じょん' }
+    ]
+  },
+  {
+    id: '2',
+    title: 'book - 2'
+  },
+  {
+    id: '3',
+    title: 'book - 3'
+  },
+];
+
 const resolvers = {
   Query: {
     user: () => ({
       id: '1',
       name: 'first last'
     }),
-    books: () => [
-      {
-        id: '1',
-        title: 'book - 1'
-      },
-      {
-        id: '2',
-        title: 'book - 2'
-      },
-      {
-        id: '3',
-        title: 'book - 3'
-      }
-    ]
+    book: (root, { id }, context) => {
+      return books.find((user) => user.id === id);
+    },
+    books: (root, { limit = 3}, context) => {
+      return books.slice(0, limit);
+    },
   }
 };
 
--- apollo/typeDefs.ts
+++ apollo/typeDefs.ts
@@ -1,19 +1,37 @@
 import { gql } from '@apollo/client'
 
 const typeDefs = gql`
+  type Query {
+    user: User!
+    book(id: ID!): Book
+    books(limit: Int): [Book!]!
+  }
+
   type User {
     id: String!
     name: String!
+    role: String!
   }
 
   type Book {
     id: String!
     title: String!
+    creators: [Creator!]
+    image: BookImage!
+    stock: Int!
+    users: [User!]
   }
 
-  type Query {
-    user: User!
-    books: [Book!]!
+  type BookImage {
+    small: String!
+    medium: String!
+    large: String!
+  }
+
+  type Creator {
+    id: String!
+    name: String!
+    role: String
   }
 `;
 
